f162ef3be2d22d0b7a6e7488ad251f8b1565002b
CubeJerry <80299683+CubeJerry@users.noreply.github.com>
2025-12-18 22:56:01 +1100

    Update predict.py

--- predict.py
+++ predict.py
@@ -8,8 +8,8 @@ import pandas as pd
 
 # does this even work?
 print("Loading ESM-2 model...")
-model, alphabet = esm.pretrained.esm2_t48_15B_UR50D()
-# model, alphabet = esm.pretrained.esm2_t36_3B_UR50D()
+# model, alphabet = esm.pretrained.esm2_t48_15B_UR50D()
+model, alphabet = esm.pretrained.esm2_t36_3B_UR50D()
 # model, alphabet = esm.pretrained.esm2_t36_650M_UR50D()
 batch_converter = alphabet.get_batch_converter()
 model.eval()  # FP32
@@ -37,8 +37,8 @@ with open(fasta_filename) as fp:
 
 # Ensure model is on the right device and pick the final representation layer
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = model.half().to(device)
-# model.to(device)
+# model = model.half().to(device)
+model.to(device)
 repr_layer = model.num_layers - 1
 
 sequence_representations_list = []
@@ -78,8 +78,8 @@ for start in range(0, len(data), chunk_size):
 X = torch.stack(sequence_representations_list, dim=0).cpu().numpy().astype(np.float32)
 
 
-keras_model_path = f"/vast/scratch/users/{os.environ['USER']}/TEMPRO/user/saved_ANNmodels_1500epoch/ESM_15B.keras"
-# keras_model_path = f"/vast/scratch/users/{os.environ['USER']}/TEMPRO/user/saved_ANNmodels_1500epoch/ESM_3B.keras"
+# keras_model_path = f"/vast/scratch/users/{os.environ['USER']}/TEMPRO/user/saved_ANNmodels_1500epoch/ESM_15B.keras"
+keras_model_path = f"/vast/scratch/users/{os.environ['USER']}/TEMPRO/user/saved_ANNmodels_1500epoch/ESM_3B.keras"
 # keras_model_path = f"/vast/scratch/users/{os.environ['USER']}/TEMPRO/user/saved_ANNmodels_1500epoch/ESM_650M.keras"
 print("Loading pretrained Keras model for Tm prediction...")
 keras_model = keras.models.load_model(keras_model_path)
